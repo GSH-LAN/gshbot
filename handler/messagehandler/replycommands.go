8d0781934b2e041887f884f7e70f9f22e2a8fe95
Feldmann, Daniel <daniel@feldmanns.email>
2024-02-02 18:04:45 +0100

    added rss part to message handler

--- handler/messagehandler/replycommands.go
+++ handler/messagehandler/replycommands.go
@@ -1,7 +1,8 @@
 package messagehandler
 
 import (
-	"log"
+	"gshlan/gshbot/extension/rss"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -13,11 +14,41 @@ func (h Handler) ReplyCommands(s *discordgo.Session, msg *discordgo.MessageCreat
 		return
 	}
 
-	// Just sample command check
-	if msg.Content == "/ping" {
-		_, err := s.ChannelMessageSend(msg.ChannelID, "pong "+h.config.Name)
-		if err != nil {
-			log.Println(err)
+	if strings.HasPrefix(msg.Content, h.config.Prefix+"rss") && len(strings.Split(msg.Content, " ")) >= 3 {
+		var channelId string
+		if strings.Split(msg.Content, " ")[1] == "add" {
+			name := strings.Split(msg.Content, " ")[2]
+			url := strings.Split(msg.Content, " ")[3]
+			if strings.Contains(url, "http://") || strings.Contains(url, "https://") {
+				s.ChannelMessageSend(msg.ChannelID, "try to add the following feed name: "+name+" url: "+url)
+				if len(strings.Split(msg.Content, " ")) == 5 {
+					channelId = strings.Split(msg.Content, " ")[4]
+				} else {
+					channelId = msg.ChannelID
+				}
+				added, returnErr := rss.AddUrlToList(name, url, channelId, h.config)
+				if added && returnErr == nil {
+					s.ChannelMessageSend(msg.ChannelID, "feed added successfully")
+				} else {
+					s.ChannelMessageSend(msg.ChannelID, "an error occurred: "+returnErr.Error())
+				}
+			} else {
+				s.ChannelMessageSend(msg.ChannelID, "please use a proper URL!")
+			}
+		} else if strings.Split(msg.Content, " ")[1] == "del" {
+			name := strings.Split(msg.Content, " ")[2]
+			if len(strings.Split(msg.Content, " ")) == 4 {
+				channelId = strings.Split(msg.Content, " ")[3]
+			} else {
+				channelId = msg.ChannelID
+			}
+			removed, removeErr := rss.RemoveFeedFromList(name, channelId, h.config)
+			if removed && removeErr == nil {
+				s.ChannelMessageSend(msg.ChannelID, "feed removed successfully")
+			} else {
+				s.ChannelMessageSend(msg.ChannelID, "an error occurred: "+removeErr.Error())
+			}
 		}
+
 	}
 }
